94144387a23cbb4914e48d7363e6b32873d9b3e3
Calvin <calvin@amaka.com>
2021-09-20 20:15:54 +1000

    clean code

--- lib/ui/views/home/home_view_model.dart
+++ lib/ui/views/home/home_view_model.dart
@@ -1,20 +1,5 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:dev_coding_test_calvin/app/core/custom_base_view_model.dart';
-import 'package:dev_coding_test_calvin/app/router/router.gr.dart';
-import 'package:flutter/material.dart';
 
 class HomeViewModel extends CustomBaseViewModel {
   Future<void> init() async {}
-
-  pushToSearchByClientNumberPage(BuildContext context) {
-    AutoRouter.of(context).push(SearchByClientNumberRoute());
-  }
-
-  pushToTotalAmountByProductAndDayPage(BuildContext context) {
-    AutoRouter.of(context).push(TotalAmountByProductAndDayRoute());
-  }
-
-  pushToDailySummaryReportPage(BuildContext context) {
-    AutoRouter.of(context).push(DailySummaryReportRoute());
-  }
 }
